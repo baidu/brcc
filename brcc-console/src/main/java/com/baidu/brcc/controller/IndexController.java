98d50a573a6008d34d3383e43639606a2fd194dd
zengqingyong <zengqingyong@appledeMacBook-Pro.local>
2021-02-04 15:53:57 +0800

    upload ui code

--- brcc-console/src/main/java/com/baidu/brcc/controller/IndexController.java
+++ brcc-console/src/main/java/com/baidu/brcc/controller/IndexController.java
@@ -7,13 +7,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baidu.brcc.domain.ConfigItemExample;
+import com.baidu.brcc.domain.ProductExample;
+import com.baidu.brcc.domain.ProjectExample;
+import com.baidu.brcc.domain.UserExample;
 import com.baidu.brcc.domain.base.R;
+import com.baidu.brcc.domain.em.Deleted;
 import com.baidu.brcc.domain.vo.NoticeVo;
+import com.baidu.brcc.service.ConfigItemService;
+import com.baidu.brcc.service.ProductService;
+import com.baidu.brcc.service.ProjectService;
+import com.baidu.brcc.service.UserService;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,19 +38,61 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("index")
 public class IndexController {
 
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    ProjectService projectService;
+
+    @Autowired
+    ConfigItemService configItemService;
+
+    @Autowired
+    UserService userService;
+
+    Cache<String, Long> cache = CacheBuilder.newBuilder().expireAfterWrite(2, TimeUnit.MINUTES).build();
+
     @GetMapping("notice")
     public R notice() {
         // TODO 新增配置功能
-        Long pv = System.currentTimeMillis()/2 * 10;
-        Map<String, Long> overview = new HashMap<String, Long>(){
-            {
-                put("产品线", 12L);
-                put("工程量", 673L);
-                put("配置项", 1292221L);
-                put("用户数", 237L);
-                put("日均访问数", pv);
-            }
-        };
+        Long productCnt = cache.getIfPresent("productCnt");
+        if (productCnt == null) {
+            productCnt = productService.countByExample(ProductExample.newBuilder().build());
+            cache.put("productCnt", productCnt);
+        }
+
+        Long projectCnt = cache.getIfPresent("projectCnt");
+        if (projectCnt == null) {
+            projectCnt = projectService.countByExample(ProjectExample.newBuilder()
+                    .build()
+                    .createCriteria()
+                    .andDeletedEqualTo(Deleted.OK.getValue())
+                    .toExample()
+            );
+            cache.put("projectCnt", projectCnt);
+        }
+
+        Long configItemCnt = cache.getIfPresent("configItemCnt");
+        if (configItemCnt == null) {
+            configItemCnt = configItemService.countByExample(ConfigItemExample.newBuilder()
+                    .build()
+                    .createCriteria()
+                    .andDeletedEqualTo(Deleted.OK.getValue())
+                    .toExample()
+            );
+            cache.put("configItemCnt", configItemCnt);
+        }
+
+        Long userCnt = cache.getIfPresent("userCnt");
+        if (userCnt == null) {
+            userCnt = userService.countByExample(UserExample.newBuilder().build());
+            cache.put("userCnt", userCnt);
+        }
+        Map<String, Long> overview = new HashMap<String, Long>();
+        overview.put("产品线数", productCnt);
+        overview.put("工程数", projectCnt);
+        overview.put("配置项数", configItemCnt);
+        overview.put("用户数", userCnt);
 
         List<String> intro = new ArrayList<>();
         intro.add("配置中心统一管理应用的资源配置信息，多是K-V结构，如memcached、" +
