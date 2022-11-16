0f50850511391087a8daf9fa48e0ede99ce83802
hardy4yooz <hardy4yooz@gmail.com>
2022-11-16 15:29:23 +0800

    fix: Uncomment the drop user ddl

--- doc/database/1.schema.sql
+++ doc/database/1.schema.sql
@@ -1,5 +1,5 @@
 drop database if exists brcc;
--- drop user if exists 'brcc'@'%';
+drop user if exists 'brcc'@'%';
 create database brcc default character set utf8mb4 collate utf8mb4_unicode_ci;
 use brcc;
 create user 'brcc'@'%' identified by 'brcc123456';
